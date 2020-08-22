f7973c47b4d8c226de66cfad6210f2981e15d317
chriscauley <chris@lablackey.com>
2020-08-22 22:15:58 +0000

    useConfig Hook and adding onChange ability to config form

--- dist/index.js
+++ dist/index.js
@@ -15,6 +15,8 @@ var _reactJsonschemaForm = _interopRequireDefault(require("@unrest/react-jsonsch
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
@@ -31,92 +33,7 @@ function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (O
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
-function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
-
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
-
-function _createSuper(Derived) { var hasNativeReflectConstruct = _isNativeReflectConstruct(); return function () { var Super = _getPrototypeOf(Derived), result; if (hasNativeReflectConstruct) { var NewTarget = _getPrototypeOf(this).constructor; result = Reflect.construct(Super, arguments, NewTarget); } else { result = Super.apply(this, arguments); } return _possibleConstructorReturn(this, result); }; }
-
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
-
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
-
-function _isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
-
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
-
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var ConfigForm = /*#__PURE__*/function (_React$Component) {
-  _inherits(ConfigForm, _React$Component);
-
-  var _super = _createSuper(ConfigForm);
-
-  function ConfigForm() {
-    var _this;
-
-    _classCallCheck(this, ConfigForm);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
-    }
-
-    _this = _super.call.apply(_super, [this].concat(args));
-
-    _defineProperty(_assertThisInitialized(_this), "state", {});
-
-    _defineProperty(_assertThisInitialized(_this), "onSubmit", function (formData) {
-      return _this.props.config.actions.save({
-        formData: formData
-      });
-    });
-
-    _defineProperty(_assertThisInitialized(_this), "onChange", function (_ref) {
-      var formData = _ref.formData;
-      return _this.setState({
-        formData: formData
-      });
-    });
-
-    return _this;
-  }
-
-  _createClass(ConfigForm, [{
-    key: "render",
-    value: function render() {
-      var _this$props = this.props,
-          config = _this$props.config,
-          props = _objectWithoutProperties(_this$props, ["config"]);
-
-      var schema = config.schema,
-          uiSchema = config.uiSchema,
-          formData = config.formData;
-      return /*#__PURE__*/_react["default"].createElement(_reactJsonschemaForm["default"], _extends({
-        formData: this.state.formData || formData,
-        schema: schema,
-        uiSchema: uiSchema,
-        onSubmit: this.onSubmit,
-        onChange: this.onChange
-      }, props));
-    }
-  }]);
-
-  return ConfigForm;
-}(_react["default"].Component);
 
 var _default = function _default(name) {
   var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
@@ -146,30 +63,67 @@ var _default = function _default(name) {
   });
   var makeHook = (0, _useGlobalHook["default"])(_react["default"], initialState, _objectSpread(_objectSpread({}, base_actions), actions));
 
+  var useConfig = function useConfig() {
+    var _makeHook = makeHook(),
+        _makeHook2 = _slicedToArray(_makeHook, 2),
+        state = _makeHook2[0],
+        actions = _makeHook2[1];
+
+    return _objectSpread(_objectSpread({
+      schema: schema,
+      uiSchema: uiSchema
+    }, state), {}, {
+      actions: actions
+    });
+  };
+
   var connect = function connect(Component) {
-    var _ref2 = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
-        _ref2$propName = _ref2.propName,
-        propName = _ref2$propName === void 0 ? og_propName : _ref2$propName;
+    var _ref = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {},
+        _ref$propName = _ref.propName,
+        propName = _ref$propName === void 0 ? og_propName : _ref$propName;
 
     return function ConfigProvider(props) {
-      var _makeHook = makeHook(),
-          _makeHook2 = _slicedToArray(_makeHook, 2),
-          state = _makeHook2[0],
-          actions = _makeHook2[1];
-
-      var connectedProps = _objectSpread(_objectSpread({}, props), {}, _defineProperty({}, propName, _objectSpread(_objectSpread({
-        schema: schema,
-        uiSchema: uiSchema
-      }, state), {}, {
-        Form: connect.Form,
-        actions: actions
-      })));
-
-      return /*#__PURE__*/_react["default"].createElement(Component, connectedProps);
+      return /*#__PURE__*/_react["default"].createElement(Component, _extends({}, props, _defineProperty({}, propName, useConfig())));
     };
   };
 
-  connect.Form = connect(ConfigForm);
+  function ConfigForm(props) {
+    var _useConfig = useConfig(),
+        schema = _useConfig.schema,
+        uiSchema = _useConfig.uiSchema,
+        formData = _useConfig.formData,
+        actions = _useConfig.actions;
+
+    var _React$useState = _react["default"].useState(formData),
+        _React$useState2 = _slicedToArray(_React$useState, 2),
+        state = _React$useState2[0],
+        setState = _React$useState2[1];
+
+    var onSubmit = function onSubmit(formData) {
+      actions.save({
+        formData: formData
+      });
+      props.onSubmit && props.onSubmit({
+        formData: formData
+      });
+    };
+
+    var onChange = function onChange(formData) {
+      props.onChange && props.onChange(formData);
+      setState(formData);
+    };
+
+    return /*#__PURE__*/_react["default"].createElement(_reactJsonschemaForm["default"], _extends({}, props, {
+      formData: state,
+      schema: schema,
+      uiSchema: uiSchema,
+      onSubmit: onSubmit,
+      onChange: onChange
+    }));
+  }
+
+  connect.useConfig = useConfig;
+  connect.Form = ConfigForm;
   return connect;
 };
 
--- src/index.js
+++ src/index.js
@@ -3,26 +3,6 @@ import globalHook from 'use-global-hook'
 import Storage from '@unrest/storage'
 import Form from '@unrest/react-jsonschema-form'
 
-class ConfigForm extends React.Component {
-  state = {}
-  onSubmit = (formData) => this.props.config.actions.save({ formData })
-  onChange = ({ formData }) => this.setState({ formData })
-  render() {
-    const { config, ...props } = this.props
-    const { schema, uiSchema, formData } = config
-    return (
-      <Form
-        formData={this.state.formData || formData}
-        schema={schema}
-        uiSchema={uiSchema}
-        onSubmit={this.onSubmit}
-        onChange={this.onChange}
-        {...props}
-      />
-    )
-  }
-}
-
 export default (name, options = {}) => {
   const { initial, schema, uiSchema, actions, propName = 'config' } = options
   const og_propName = propName
@@ -44,24 +24,45 @@ export default (name, options = {}) => {
     ...actions,
   })
 
-  const connect = (Component, { propName = og_propName } = {}) => {
-    return function ConfigProvider(props) {
-      const [state, actions] = makeHook()
-      const connectedProps = {
-        ...props,
-        [propName]: {
-          schema,
-          uiSchema,
-          ...state,
-          Form: connect.Form,
-          actions,
-        },
-      }
-      return <Component {...connectedProps} />
+  const useConfig = () => {
+    const [state, actions] = makeHook()
+    return {
+      schema,
+      uiSchema,
+      ...state,
+      actions,
     }
   }
 
-  connect.Form = connect(ConfigForm)
+  const connect = (Component, { propName = og_propName } = {}) => {
+    return function ConfigProvider(props) {
+      return <Component {...props} {...{ [propName]: useConfig() }} />
+    }
+  }
 
+  function ConfigForm(props) {
+    const { schema, uiSchema, formData, actions } = useConfig()
+    const [state, setState] = React.useState(formData)
+    const onSubmit = (formData) => {
+      actions.save({ formData })
+      props.onSubmit && props.onSubmit({ formData })
+    }
+    const onChange = (formData) => {
+      props.onChange && props.onChange(formData)
+      setState(formData)
+    }
+    return (
+      <Form
+        {...props}
+        formData={state}
+        schema={schema}
+        uiSchema={uiSchema}
+        onSubmit={onSubmit}
+        onChange={onChange}
+      />
+    )
+  }
+  connect.useConfig = useConfig
+  connect.Form = ConfigForm
   return connect
 }
